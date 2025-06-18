ca176c62db0d9001eb70ffeedbc52d53dd3b994a
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2025-06-17 21:45:27 -0300

    Aula 06 - Criando o estado de troca de botões

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,18 +1,37 @@
+import { useState } from 'react';
 import * as S from './styled';
 
-const Task = () => (
-    <li>
+type PropsTask = {
+    title: string;
+    priority: string;
+    status: string;
+    description: string;
+};
+
+const Task = ({ title, priority, status, description }: PropsTask) => {
+    const [edit, setEdit] = useState(false);
+
+    return (
         <S.Card>
-            <S.Title>Tarefas X</S.Title>
-            <S.Tag>Importante</S.Tag>
-            <S.Tag>Pendente</S.Tag>
-            <S.Description />
+            <S.Title>{title}</S.Title>
+            <S.Tag>{priority}</S.Tag>
+            <S.Tag>{status}</S.Tag>
+            <S.Description value={description} readOnly={!edit} />
             <S.ActionsBar>
-                <S.Button>Editar</S.Button>
-                <S.Button>Remover</S.Button>
+                {edit ? (
+                    <>
+                        <S.Button>Salvar</S.Button>
+                        <S.Button onClick={() => setEdit(false)}>Cancelar</S.Button>
+                    </>
+                ) : (
+                    <>
+                        <S.Button onClick={() => setEdit(true)}>Editar</S.Button>
+                        <S.Button>Remover</S.Button>
+                    </>
+                )}
             </S.ActionsBar>
         </S.Card>
-    </li>
-);
+    )
+};
 
 export default Task;
--- src/containers/TaskList/index.tsx
+++ src/containers/TaskList/index.tsx
@@ -1,19 +1,16 @@
 import Task from "../../components/Task";
 import * as S from "./styled";
+import tasks from "../../JSON/task.json";
 
 const TaskList = () => (
     <S.Main>
         <p>2 tarefas marcadas como "todas" e "termo".</p>
         <ul>
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
-            <Task />
+            {tasks.map(({ title, priority, status, description }) => (
+                <li key={title}>
+                    <Task title={title} priority={priority} status={status} description={description} />
+                </li>
+            ))}
         </ul>
     </S.Main>
 );
