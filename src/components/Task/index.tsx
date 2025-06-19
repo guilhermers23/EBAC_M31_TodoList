4affc9eb6f88f90d5138743649c3979ea885d1a5
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2025-06-19 11:49:51 -0300

    Parte 2 | Aula 02 - Editando uma tarefa

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,26 +1,51 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { removeTask } from '../../store/reducers/tasks';
+import { removeTask, editTask } from '../../store/reducers/tasks';
 import type TaskClass from '../../models/Task';
 import * as S from './styled';
 
 type PropsTask = TaskClass;
 
-const Task = ({ id, title, priority, status, description }: PropsTask) => {
+const Task = ({ id, title, priority, status, description: originalDescription }: PropsTask) => {
     const dispatch = useDispatch();
+    const [newDescription, setNewDescription] = useState(originalDescription);
     const [edit, setEdit] = useState(false);
+
+    // useEffect(() => {
+    //     if (originalDescription.length > 0) {
+    //         setNewDescription(originalDescription);
+    //     }
+    // }, [originalDescription])
+
+    const cancelEdit = () => {
+        setEdit(false);
+        setNewDescription(originalDescription);
+    };
+
+    const saveEdit = () => {
+        dispatch(editTask({
+            id,
+            description: newDescription,
+            title,
+            priority,
+            status
+        }));
+        setEdit(false);
+    };
 
     return (
         <S.Card>
             <S.Title>{title}</S.Title>
             <S.Tag parameter='Priority' priorityColor={priority}>{priority}</S.Tag>
             <S.Tag parameter='Status' statusColor={status}>{status}</S.Tag>
-            <S.Description value={description} readOnly={!edit} />
+            <S.Description value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                readOnly={!edit} />
             <S.ActionsBar>
                 {edit ? (
                     <>
-                        <S.SaveButton>Salvar</S.SaveButton>
-                        <S.RedButton onClick={() => setEdit(false)}>Cancelar</S.RedButton>
+                        <S.SaveButton onClick={saveEdit}>Salvar</S.SaveButton>
+                        <S.RedButton onClick={cancelEdit}>Cancelar</S.RedButton>
                     </>
                 ) : (
                     <>
--- src/store/reducers/tasks.ts
+++ src/store/reducers/tasks.ts
@@ -1,5 +1,6 @@
 import { data, type TaskState } from "../../db/dataTasks";
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type Task from "../../models/Task";
 
 const initialState: TaskState = data;
 
@@ -12,8 +13,16 @@ const taskSSlice = createSlice({
         (task) => task.id !== action.payload
       );
     },
+    editTask: (state, action: PayloadAction<Task>) => {
+      const indexTask = state.taskList.findIndex(
+        (t) => t.id === action.payload.id
+      );
+      if (indexTask >= 0) {
+        state.taskList[indexTask] = action.payload;
+      }
+    },
   },
 });
 
-export const { removeTask } = taskSSlice.actions;
+export const { removeTask, editTask } = taskSSlice.actions;
 export default taskSSlice.reducer;
